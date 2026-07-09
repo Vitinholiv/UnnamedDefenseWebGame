943567e853cabd79a891fb26c5ff12f83a4cb376
VitinDenoyr <vitordenoyr@gmail.com>
2026-07-08 21:24:40 -0300

    feat: Server and BD

---
+++ api/env_example.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('THIS_STRING_IS_SECRET_LOL_I_WILL_CHANGE_IT')) { exit('Acesso negado'); }
+return [
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'exemplo_banco',
+    'DB_USER' => 'exemplo_usuario',
+    'DB_PASS' => 'exemplo_senha'
+];
